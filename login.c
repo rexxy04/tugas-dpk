76f3c8fc51923df6ae8b1f6491ea7845930e7e6c
rexxy04 <143421670+rexxy04@users.noreply.github.com>
2023-10-30 13:38:36 +0800

    Update login (success).c
    All logic problem solved

--- login.c
+++ login.c
@@ -1,31 +1,33 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
-int main(){
+int main() {
     const char usn[] = {"Admin"};
     const char pwd[] = {"User"};
 
     char useri[100];
     char pwi[100];
-    int cekusn;
-    int cekpw;
 
     printf("Username: ");
     fgets(useri, sizeof useri, stdin);
     printf("Password: ");
-    scanf(pwi, sizeof pwi, stdin);
-    strtok(useri, "\n");
-    strtok(pwi, "\n");
+    fgets(pwi, sizeof pwi, stdin);
 
-    if (strcmp(usn, useri) && strcmp(pwd, pwi) == 0)
-    {
-        printf("Selamat datang %s", useri);
-    }else if (strcmp (usn, useri) && strcmp(pwd, pwi) < 0)
-    {
-        printf("Pw Salah!");
-    }else{
-        printf("Parameter error");
+    strtok(useri, "\n"); //untuk issue ketika klik enter terbaca karakter
+    strtok(pwi, "\n"); //untuk issue ketika klik enter terbaca karakter
+
+    int cekusn = strcmp(usn, useri);
+    int cekpw = strcmp(pwd, pwi);
+
+    if (cekusn == 0 && cekpw == 0) {
+        printf("Selamat datang %s\n", useri);
+    } else if (cekusn != 0 && cekpw == 0) {
+        printf("Username salah!\n");
+    } else if (cekusn == 0 && cekpw != 0) {
+        printf("Password salah!\n");
+    } else {
+        printf("Parameter error\n");
     }
 
-return 0; 
+    return 0;
 }
